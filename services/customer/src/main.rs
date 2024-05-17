732731bcc18fe410982ba66f77ee01de4e79849e
Thanaphoom Babparn <tp.babparn@gmail.com>
2024-05-17 17:55:24 +0900

    Add customer project

---
+++ services/customer/src/main.rs
@@ -0,0 +1,17 @@
+use actix_web::{App, get, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello from Customer!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+        .bind(("0.0.0.0", 8080))?
+        .run()
+        .await
+}
